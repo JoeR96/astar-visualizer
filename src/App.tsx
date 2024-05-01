882257dfee5a379a5960c8308d58ecc918419597
BigDave <joeyrichardson96@gmail.com>
2024-05-01 19:58:18 +0100

    Update some styling + add title

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Layout } from './Layout/Layout';
 function App() {
   return (
     <Fragment>
+      <h1 className="main-heading">A* Visualizer</h1>  // Corrected and added a class for styling
       <Layout />
     </Fragment>
   );
